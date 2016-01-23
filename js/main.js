bafbdab41c6b1be2439a660af511e9e676f779f0
Moises Silva <mogosilva@gmail.com>
2016-01-23 17:15:23 -0500

    Site appearance
    changed from Google Maps to MapBox
    Also adjusted the styling to be more appealing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-var mapOptions = {
-    center: new google.maps.LatLng(42.526909, -70.898427),
-    minZoom: 10,
-    zoom: 18,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+//Code for MapBox
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9nb3NpbHZhIiwiYSI6ImNpanJrOHQxZjBicml1NG01eHRuYTJ2d3oifQ.KUmx-Iy2D73MnUkYFrXw3Q';
 
-new google.maps.Map(document.getElementById('map'), mapOptions);
-
-var markerOptions = {
-    position: new google.maps.LatLng(42.526909, -70.898427)
-};
-var marker = new google.maps.Marker(markerOptions);
-marker.setMap(map);
+//Check to see if MapBox is subborted or user's device
+if (!mapboxgl.supported()) {
+    
+    alert('Your browser does not support Mapbox GL');
+} 
+else {
+    
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+        center: [-74.50, 40], //starting location
+        zoom: 9 //starting zoom
+    
+    });
+}
