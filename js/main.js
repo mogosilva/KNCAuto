20fb149c265e8b4714587030a914c07a916adf0a
Moises Silva <mogosilva@gmail.com>
2016-03-16 12:22:56 -0400

    hover to left and right arrows in #hero

--- js/main.js
+++ js/main.js
@@ -1,40 +1,65 @@
 var images=["images/image_01.jpg","images/image_02.jpg"];
 var i=0;
+var heroImage=$("#hero section");
+var counter=5000;
+var autoFlip = window.setInterval(nextImage, 5000);
 
 function previousImage(){
+	
+	clearInterval(autoFlip);
+	autoFlip = window.setInterval(nextImage, 5000);
 	if(i<images.length && i>0){
 		i--;
-		$(".hero section").css("background","url(" + images[i] + ")");
-		$(".hero section").css("background-size","cover");
+		heroImage.css("background","url(" + images[i] + ")");
+		heroImage.css("background-size","cover");
 		
 	}else{
 		i=1;
-		$(".hero section").css("background","url(" + images[i] + ")");
-		$(".hero section").css("background-size","cover");
+		heroImage.css("background","url(" + images[i] + ")");
+		heroImage.css("background-size","cover");
 	}
 }
 function nextImage(){
 	//i = i + 1;
+	clearInterval(autoFlip);
+	autoFlip = window.setInterval(nextImage, 5000);
 
 	if(i<images.length-1){
 		i++;
-		$(".hero section").css("background","url(" + images[i] + ")");
-		$(".hero section").css("background-size","cover");
+		heroImage.css("background","url(" + images[i] + ")");
+		heroImage.css("background-size","cover");
 	}else{
 		i=0;
-		$(".hero section").css("background","url(" + images[i] + ")");
-		$(".hero section").css("background-size","cover");
+		heroImage.css("background","url(" + images[i] + ")");
+		heroImage.css("background-size","cover");
 	}
 }
 
+
+
+
+
+
+
+
 $(document).ready(function(){
-	$(".btn[value='>']").on("click",nextImage);
-	$(".btn[value='<']").on("click",previousImage);
+	$("img[value='>']").on("click",nextImage);
+	$("img[value='<']").on("click",previousImage);
+
+	$(".rightArrow").hover(function(){
+		$(".rightArrow").toggleClass("hidden");
+	});
+
+	$(".leftArrow").hover(function(){
+		$(".leftArrow").toggleClass("hidden");
+	});
+
+
 
 	$("#dealsLink").on("click",function(e){
 		 e.preventDefault();
 		$("body").animate({
-			scrollTop: $(".deals").offset().top - 100
+			scrollTop: $("#deals").offset().top - 100
 		},1000);
 		
 	});
@@ -62,10 +87,6 @@ $(document).ready(function(){
 		},1000);
 		
 	});
-
-	window.setInterval(function(){
-  		 nextImage();
-	}, 5000);
 
 	$("#studentId").mouseenter(function(){
 		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -126,24 +147,24 @@ var map;
           scrollwheel: false,
         });
 
-var infowindow = new google.maps.InfoWindow();
-var service = new google.maps.places.PlacesService(map);
+		var infowindow = new google.maps.InfoWindow();
+		var service = new google.maps.places.PlacesService(map);
 
-service.getDetails({
-    placeId: 'ChIJUfy2B3EU44kRXWCSXwZWwGs'
-  }, function(place, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
-          place.formatted_address + '</div>');
-        infowindow.open(map, this);
-      });
+		service.getDetails({
+			placeId: 'ChIJUfy2B3EU44kRXWCSXwZWwGs'
+		  	}, function(place, status) {
+			if (status === google.maps.places.PlacesServiceStatus.OK) {
+				var marker = new google.maps.Marker({
+				map: map,
+				position: place.geometry.location
+				});
+				google.maps.event.addListener(marker, 'click', function() {
+				infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+				place.formatted_address + '</div>');
+				infowindow.open(map, this);
+				});
+		    }
+		  });
     }
-  });
-      }
 
 
